3122f4e326fab8e719851330c3c81742e38d6cee
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-16 13:53:21 +0100

    fix: resolved testing of client components
    Resolved the issue of testing client components that use hooks like useState, **temporarily**. This approach still produces a warning when executing tests, but works for now:
    
    ```
    Warning: Render methods should be a pure function of props and state; triggering nested component updates from render is not allowed. If necessary, trigger nested updates in componentDidUpdate.
    
    Check the render method of TestComponent.
    ```

--- components/ClientComponent.tsx
+++ components/ClientComponent.tsx
@@ -1,12 +1,9 @@
 'use client'
 
 import { useState } from 'react'
-import { renderHook } from '@testing-library/react'
 
 export default function ClientComponent() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const state = renderHook(() => useState(false))
-  const [isOpen, setIsOpen] = state.result.current
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   return <h1>This is a client component...</h1>
 }
--- tests/jest/root/ClientComponent.test.tsx
+++ tests/jest/root/ClientComponent.test.tsx
@@ -1,10 +1,9 @@
 import { it } from '@jest/globals'
-import { render, screen } from '@testing-library/react'
+import { render, renderHook, screen } from '@testing-library/react'
 import ClientComponent from '@/components/ClientComponent'
 
 it('test client component', async () => {
-  const Component = await ClientComponent()
-  render(Component)
+  renderHook(() => render(ClientComponent()))
 
   const heading = screen.getByRole('heading', { level: 1 })
   expect(heading).toBeInTheDocument()
