7a623b9fbdc27dced48d073b33debe2d9a31f034
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-16 13:50:49 +0100

    feat: showcase exemplary use of renderHook inside client-component
    One way of **partly** solving the issue is through the use of the `renderHook` function. However, this workaround only works for test-cases only. This means that it cannot be used inside of components that are actually being rendered.

--- components/ClientComponent.tsx
+++ components/ClientComponent.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import { useState } from 'react'
+import { renderHook } from '@testing-library/react'
 
 export default function ClientComponent() {
-  const [mode, setMode] = useState(false)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const state = renderHook(() => useState(false))
+  const [isOpen, setIsOpen] = state.result.current
 
   return <h1>This is a client component...</h1>
 }
