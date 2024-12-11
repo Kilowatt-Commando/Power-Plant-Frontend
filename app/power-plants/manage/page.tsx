f18ec79cbaa320e39e11759d78dfeb9fdb145adf
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-11 16:54:27 +0100

    feat: added Table of PowerPlants to /manage page
    The /manage page now displays PowerPlants using the Table component.

--- app/power-plants/manage/page.tsx
+++ app/power-plants/manage/page.tsx
@@ -1,3 +1,23 @@
+import PowerPlantProps, { createDummyPowerPlants } from '@/typings/Shared/PowerPlant'
+import React from 'react'
+import { Table } from '@/components/Shared/Table/Table'
+import ManagePowerPlantActionButtons from '@/app/power-plants/manage/ManagePowerPlantActionButtons'
+
 export default async function ManagePowerPlantsPage() {
-  return <div>Manage Power Plants</div>
+  const plants = createDummyPowerPlants(20)
+
+  return (
+    <div>
+      <h1 className='text-2xl font-semibold mb-8 mt-2 tracking-wide'>Manage Power Plants</h1>
+
+      <Table<PowerPlantProps>
+        items={plants}
+        filterDisplayedProperties={['id', 'name', 'capacity']}
+        propertyClasses={{
+          name: 'text-blue-500',
+        }}
+        actionButtons={ManagePowerPlantActionButtons}
+      />
+    </div>
+  )
 }
