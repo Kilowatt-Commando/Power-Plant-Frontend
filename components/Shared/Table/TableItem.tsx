f90b60e8fb31874fee458c51adbc8beb6298456c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-11 16:51:34 +0100

    ref: updated rendering of table items to use context
    Changed the rendering of table items by using the new `TableRowAnimationContext` component. This means that the tr tag is replaced by the Context component.
    
    This way, the `actionButtons` that may be provided to the Table component can access the variables provided by the context.

--- components/Shared/Table/TableItem.tsx
+++ components/Shared/Table/TableItem.tsx
@@ -1,21 +1,15 @@
-'use client'
-
-import { useAnimate } from 'framer-motion'
-import { twMerge } from 'tailwind-merge'
 import React from 'react'
-import { TableActionButtonProps, TableProps } from '@/components/Shared/Table/Table'
+import { TableProps } from '@/components/Shared/Table/Table'
+import TableRowAnimationContextProvider from '@/components/Shared/Table/TableRowAnimationContextProvider'
+import { twMerge } from 'tailwind-merge'
 
 export interface TableItemProps<T = object> extends Required<Omit<TableProps<T>, 'items'>> {
   item: T
 }
 
-export default function TableItem<T>({ item, filterDisplayedProperties, ...props }: TableItemProps<T>) {
-  const [scope, animate] = useAnimate()
-
+export default function TableItem<T>({ item, filterDisplayedProperties, actionButtons: ActionButtons, ...props }: TableItemProps<T>) {
   return (
-    <tr
-      key={Math.random()}
-      ref={scope}
+    <TableRowAnimationContextProvider
       className={twMerge(
         'h-12 transition-colors duration-200 dark:text-gray-300 border-neutral-400  dark:border-neutral-500 dark:hover:bg-neutral-700  shadow-md shadow-neutral-300 dark:shadow-neutral-900',
       )}>
@@ -24,17 +18,9 @@ export default function TableItem<T>({ item, filterDisplayedProperties, ...props
           {item[property]}
         </td>
       ))}
-      <ItemActionButtons item={item} animate={animate} scope={scope} {...props} />
-    </tr>
-  )
-}
-
-function ItemActionButtons<T>({ item, animate, scope, actionButtons: ActionButtons }: TableActionButtonProps<T> & Required<TableProps<T>>) {
-  if (!ActionButtons) return null
-
-  return (
-    <td className='flex justify-center h-12 gap-4 p-1.5'>
-      <ActionButtons item={item} animate={animate} scope={scope} />
-    </td>
+      <td className={twMerge('flex justify-center h-12 gap-4 p-1.5', ActionButtons ? '' : 'hidden')}>
+        <ActionButtons item={item} />
+      </td>
+    </TableRowAnimationContextProvider>
   )
 }
