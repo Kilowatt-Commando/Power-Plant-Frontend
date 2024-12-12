2612d35f9b728307f6fc7dad5d1f128835cd20de
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-12 12:15:08 +0100

    ref: updated types of default Table properties

--- hooks/useTableProps.ts
+++ hooks/useTableProps.ts
@@ -1,9 +1,11 @@
 import { TableProps } from '@/components/Shared/Table/Table'
+import getObjectKeys from '@/lib/getObjectKeys'
 
 export default function useTableProps<T = object>({ ...props }: TableProps<T>): Required<TableProps<T>> {
   const defaultProps: Required<TableProps<T>> = {
-    filterDisplayedProperties: Object.keys(props.items.at(0)),
-    propertyClasses: {},
+    filterDisplayedProperties: getObjectKeys<T>(props.items.at(0)),
+    propertyClasses: {} as { [key in keyof Partial<T>]: string },
+    actionButtons: () => null,
     ...props,
   }
 
