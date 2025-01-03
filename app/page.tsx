f2fffdaf21dfee560f38fc17b8e4c30ceb427239
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-03 12:21:49 +0100

    ref: updated root page to show Animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import AnimatedPowerPlant from '@/components/root/PowerPlantAnimation/AnimatedPowerPlant'
+
 export default async function Home() {
   return (
-    <div className=''>
-      <h1 className='text-2xl '>Root Page..</h1>
-    </div>
+    <>
+      <h1 className='text-2xl font-semibold'>Power Plant Workflow</h1>
+      <AnimatedPowerPlant />
+    </>
   )
 }
