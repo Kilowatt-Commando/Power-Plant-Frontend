9495bf48fbf897ae43120b110f268faa67d6f656
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-06 12:26:17 +0100

    feat: created MobileSideBarContextProvider component
    This component is used to share state through the use of contexts.

---
+++ components/root/SideBar/variants/MobileSideBarContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from 'react'
+
+interface MobileSideBarContextProps {
+  isOpen: boolean
+  setIsOpen: (value: boolean) => void
+}
+
+const MobileSideBarContext = createContext<MobileSideBarContextProps>({} as MobileSideBarContextProps)
+
+export function useMobileSideBarContext() {
+  const context = useContext(MobileSideBarContext)
+
+  if (!context) {
+    throw new Error('useMobileSideBarContext must be used within a MobileSideBarContextProvider')
+  }
+
+  return context
+}
+
+export default function MobileSideBarContextProvider({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return <MobileSideBarContext.Provider value={{ isOpen, setIsOpen }}>{children}</MobileSideBarContext.Provider>
+}
