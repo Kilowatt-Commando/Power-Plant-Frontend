7e5f0b3c1c1fa7a14e47aea27009cd4a8492b9ae
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-06 12:13:48 +0100

    ref: updated getColorMode lib module
    The function now returns the current colormode from the cookies or undefined, if no cookie is found.

--- lib/getColorMode.ts
+++ lib/getColorMode.ts
@@ -1,8 +1,10 @@
 import { cookies as nextCookies } from 'next/dist/client/components/headers'
 
-export default function getColorMode(): 'light' | 'dark' {
+export default function getColorMode(): 'light' | 'dark' | undefined {
   const cookies = nextCookies()
   const colorMode = cookies.get('colorMode' as never)?.value
+
+  if (!colorMode) return undefined
 
   return colorMode === 'light' ? 'light' : 'dark'
 }
