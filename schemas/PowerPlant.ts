136f3232525649705732b2f5a17c1257c26aeeac
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-06 10:14:50 +0100

    ref: updated naming and docs of internal function

--- schemas/PowerPlant.ts
+++ schemas/PowerPlant.ts
@@ -10,17 +10,22 @@ const PowerPlantSchema = z.object({
 
 export type PowerPlant = z.infer<typeof PowerPlantSchema>
 
-const getRandomInt = (min = 0, max = 100) => {
+/**
+ * This function generates a random number between min and max, by default between 0 and 100
+ * @param min - minimum value, by default 0
+ * @param max - maximum value, by default 100
+ */
+const getRandomValue = (min = 0, max = 100) => {
   return (Math.floor(Math.random() * max) + min) % max
 }
 
 export function createDummyPowerPlants(amount: number): PowerPlant[] {
   return Array.from({ length: amount }).map(() => ({
-    id: getRandomInt(),
+    id: getRandomValue(),
     name: 'Dummy Power Plant',
-    waterThroughput: getRandomInt(),
-    rpm: getRandomInt(0, 1000),
-    outputVoltage: getRandomInt(100, 230),
+    waterThroughput: getRandomValue(),
+    rpm: getRandomValue(0, 1000),
+    outputVoltage: getRandomValue(100, 230),
   }))
 }
 
