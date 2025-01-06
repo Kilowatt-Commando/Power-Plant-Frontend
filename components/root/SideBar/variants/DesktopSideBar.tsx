1bdf0785f102dd44338e97b262421fbce6f1c27e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-06 12:20:25 +0100

    ref: added colorMode to ColorModeSwitcher in DesktopSideBar
    Passed the default colorMode to the ColorModeSwitcher component in the DesktopSidebar. This way the icon is not animated on the initial render.

--- components/root/SideBar/variants/DesktopSideBar.tsx
+++ components/root/SideBar/variants/DesktopSideBar.tsx
@@ -6,13 +6,16 @@ import { RenderSideBarItems } from '@/components/root/SideBar'
 import { LoadingProfileInformation } from '@/components/root/UserProfile/LoadingUserInformation'
 import UserProfile from '@/components/root/UserProfile/UserProfie'
 import ColorModeSwitcher from '@/components/root/ColorModeSwitcher'
+import getColorMode from '@/lib/getColorMode'
 
-export default function DesktopSideBar(props: SideBarProps) {
+export default async function DesktopSideBar(props: SideBarProps) {
+  const colorMode = await getColorMode()
+
   return (
     <div id='desktop-sidebar-container' className={twMerge('fixed dark:bg-neutral-900/50 bg-gray-200 h-full flex-col p-3 pl-2', DesktopSideBarVisibilityBreakpoints)}>
       <div id={'sidebar-header'} className='flex gap-4 items-center justify-center pb-3 border-b-2 dark:border-neutral-300 border-gray-600'>
         <h3 className='text-xl dark:text-gray-300 text-gray-700'>{props.title}</h3>
-        <ColorModeSwitcher />
+        <ColorModeSwitcher defaultValue={colorMode} />
       </div>
 
       <div className='h-full dark:text-gray-300/90 text-gray-600 py-4'>
