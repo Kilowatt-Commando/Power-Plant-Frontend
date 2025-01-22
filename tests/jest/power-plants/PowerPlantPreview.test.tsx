09c17b730896df64bb51f23fdb52f5a42da08dce
MrsicMarko <marko2605@gmx.at>
2025-01-23 00:44:29 +0100

    fix: change testclass

--- tests/jest/power-plants/PowerPlantPreview.test.tsx
+++ tests/jest/power-plants/PowerPlantPreview.test.tsx
@@ -8,8 +8,21 @@ const dummyPowerPlants = createDummyPowerPlants(5)
 describe('PowerPlantPreview - ', () => {
   it('has correct redirect link', async () => {
     const href = '/test'
-    const Component = await PowerPlantPreview({ ...dummyPowerPlants.at(0), previewHref: href })
-    const { container } = render(Component)
+    const powerPlant = dummyPowerPlants.at(0)
+    const encodedPowerPlant = encodeURIComponent(
+      JSON.stringify({
+        id: powerPlant.id,
+        name: powerPlant.name,
+        status: powerPlant.status,
+        rpm: powerPlant.rpm,
+        outputVoltage: powerPlant.outputVoltage,
+        waterThroughput: powerPlant.waterThroughput,
+        nextWeather: powerPlant.nextWeather,
+        timestamp: powerPlant.timestamp,
+      }),
+    )
+
+    const { container } = render(<PowerPlantPreview {...powerPlant} previewHref={href} />)
 
     const links = container.getElementsByTagName('a')
     expect(links.length).toBeGreaterThanOrEqual(1)
@@ -17,20 +30,20 @@ describe('PowerPlantPreview - ', () => {
     const link = links[0]
 
     expect(link).toBeInTheDocument()
-    expect(link).toHaveAttribute('href', href)
+    expect(link).toHaveAttribute('href', `${href}?powerplant=${encodedPowerPlant}`)
   })
 
   it('displays correct power plant name and id', async () => {
-    const Component = await PowerPlantPreview({ ...dummyPowerPlants.at(0) })
-    const { container } = render(Component)
+    const powerPlant = dummyPowerPlants.at(0)
+    const { container } = render(<PowerPlantPreview {...powerPlant} />)
 
     const nameHeading = container.getElementsByTagName('h3')[0]
     const idSpan = container.getElementsByTagName('span')[0]
 
     expect(nameHeading).toBeInTheDocument()
-    expect(nameHeading.textContent).toBe(dummyPowerPlants.at(0).name)
+    expect(nameHeading.textContent).toBe(powerPlant.name)
 
     expect(idSpan).toBeInTheDocument()
-    expect(idSpan.textContent).toBe(`#${dummyPowerPlants.at(0).id}`)
+    expect(idSpan.textContent).toBe(`#${powerPlant.id}`)
   })
 })
