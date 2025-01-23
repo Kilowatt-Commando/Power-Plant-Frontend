3f7fb8bee7807d6c1bfcaf422914eac3ee140eb8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-01-23 16:49:40 +0100

    feat: initialized EnvironmentVariables provider in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import AuthProvider from '@/components/root/AuthProvider'
 import getColorMode from '@/lib/getColorMode'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
+import EnvironmentVariablesProvider from '@/components/power-plants/manage/EnvironmentVariablesProvider'
 
 export const metadata: Metadata = {
   title: 'Power Plant Frontend',
@@ -26,7 +27,9 @@ export default function RootLayout({
       <body className={`bg-gray-100 dark:bg-neutral-800 text-gray-700 dark:text-gray-200`}>
         <SideBar />
         <AuthProvider>
-          <div className={twMerge('p-4 @container', MainContentShiftBreakpoints)}>{children}</div>
+          <EnvironmentVariablesProvider DATA_API={process.env.DATA_API} CONTROL_API={process.env.CONTROL_API}>
+            <div className={twMerge('p-4 @container', MainContentShiftBreakpoints)}>{children}</div>
+          </EnvironmentVariablesProvider>
         </AuthProvider>
         <ToastContainer position='top-right' autoClose={3000} stacked pauseOnFocusLoss={false} />
       </body>
